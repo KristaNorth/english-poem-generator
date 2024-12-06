57f54441429b2ff06a733e7a30a31391ffd6bc09
KristaNorth <165333028+KristaNorth@users.noreply.github.com>
2024-12-06 15:46:05 -0600

    Added context, prompt and further instructions in JS

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function displayPoem(response) {
-    console.log{"poem generated"};
+  console.log("poem generated");
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -13,13 +13,14 @@ function generatePoem(event) {
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "e1676b45c78tcc0f65e93o16f7bcf3a0";
- let context = "You are an English language poem expert and love to write short poems. Your mission is to generate a four line poem in basic HTML. Make sure to follow the user instructions below.";
+  let context =
+    "You are an English language poem expert and love to write short poems. Your mission is to generate a four line poem using basic HTML and separate each line with a <br />. Please follow the instructions below and omit 'html' at beginning of the poem. Sign the poem with 'She Codes AI' inside a <strong> element at the bottom of the poem";
   let prompt = `User instructions: Generate an English poem about ${instructionsInput.value}`;
- let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log{"Generating poem"};
-  console.log{`Prompt: ${prompt}`};
-  console.log{`Context: ${context}`};
+  console.log("Generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
   // make a call to the API in axios
   axios.get(apiURL).then(displayPoem);
   // display the generated poem
